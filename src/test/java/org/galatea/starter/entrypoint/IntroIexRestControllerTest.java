1393e5a09ff91d8755fba3d71a1e73a8cff96d69
nlisiecka <nlisiecka@ufl.edu>
2022-02-01 08:24:37 -0500

    moved intro project changes into separate classes/files

--- src/test/java/org/galatea/starter/entrypoint/IntroIexRestControllerTest.java
+++ src/test/java/org/galatea/starter/entrypoint/IntroIexRestControllerTest.java
@@ -5,11 +5,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
-import java.util.Collections;
 import junitparams.JUnitParamsRunner;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.galatea.starter.ASpringTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +17,6 @@ import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
 
 @RequiredArgsConstructor
 @Slf4j
@@ -31,54 +28,27 @@ import org.springframework.test.web.servlet.MvcResult;
 // we previously used WireMockClassRule for consistency with ASpringTest, but when moving to a dynamic port
 // to prevent test failures in concurrent builds, the wiremock server was created too late and feign was
 // already expecting it to be running somewhere else, resulting in a connection refused
-@AutoConfigureWireMock(port = 0, files = "classpath:/wiremock")
+@AutoConfigureWireMock(port = 0, files = "classpath:/intro-wiremock")
 // Use this runner since we want to parameterize certain tests.
 // See runner's javadoc for more usage.
 @RunWith(JUnitParamsRunner.class)
-public class IexRestControllerTest extends ASpringTest {
+public class IntroIexRestControllerTest {
 
   @Autowired
   private MockMvc mvc;
-
-  @Test
-  public void testGetSymbolsEndpoint() throws Exception {
-    MvcResult result = this.mvc.perform(
-        // note that we were are testing the fuse REST end point here, not the IEX end point.
-        // the fuse end point in turn calls the IEX end point, which is WireMocked for this test.
-        org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/iex/symbols")
-            .accept(MediaType.APPLICATION_JSON_VALUE))
-        .andExpect(status().isOk())
-        // some simple validations, in practice I would expect these to be much more comprehensive.
-        .andExpect(jsonPath("$[0].symbol", is("A")))
-        .andExpect(jsonPath("$[1].symbol", is("AA")))
-        .andExpect(jsonPath("$[2].symbol", is("AAAU")))
-        .andReturn();
-  }
 
   @Test
   public void testGetLastTradedPrice() throws Exception {
 
     MvcResult result = this.mvc.perform(
         org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-            .get("/iex/lastTradedPrice?symbols=FB")
+            .get("/iex/lastTradedPrice?symbols=AAPL")
             // This URL will be hit by the MockMvc client. The result is configured in the file
-            // src/test/resources/wiremock/mappings/mapping-lastTradedPrice.json
+            // src/test/resources/intro-wiremock/mappings/mapping-introLastTradedPrice.json
             .accept(MediaType.APPLICATION_JSON_VALUE))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$[0].symbol", is("FB")))
         .andExpect(jsonPath("$[0].price").value(new BigDecimal("186.34")))
-        .andReturn();
-  }
-
-  @Test
-  public void testGetLastTradedPriceEmpty() throws Exception {
-
-    MvcResult result = this.mvc.perform(
-        org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-            .get("/iex/lastTradedPrice?symbols=")
-            .accept(MediaType.APPLICATION_JSON_VALUE))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$", is(Collections.emptyList())))
         .andReturn();
   }
 }
